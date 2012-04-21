2041eb3a904d29e71ba660c62d3146b6f992dabc
abars <abarsceo@gmail.com>
2012-04-21 11:29:03 +0900

    Fix image response not found problem

--- js/thread.js
+++ js/thread.js
@@ -43,7 +43,7 @@
 				width=document.getElementById("canvas_width").value;
 				height=document.getElementById("canvas_height").value;
 			}
-			if(illust_mode==2){
+			if(illust_mode==2 && !reply_mode){
 				window.location.href=''+host+'draw_moper?thread_key='+thread_key+'&bbs_key='+bbs_key+'&canvas_url='+url;
 			}else{
 				window.location.href=''+host+'draw?thread_key='+thread_key+'&bbs_key='+bbs_key+'&canvas_width='+width+"&canvas_height="+height+"&canvas_url="+url+"&reply="+reply_mode;
