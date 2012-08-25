f59562514aed2ad5df1bf235280b18a49eebd6a8
abars <abarsceo@gmail.com>
2012-08-25 23:12:10 +0900

    refine html tag

--- templatetags/django_filter.py
+++ templatetags/django_filter.py
@@ -46,29 +46,8 @@ def time_JST_short(value):
 	return time_JST_format(value,10)
 	
 #-----------------------------------------------------------------
-#ランキング表記
+#絵で返信
 #-----------------------------------------------------------------
-
-#def ranking_image_with_except(ranking,host):
-#	if(ranking["short"] and ranking["short"]!="None") :
-#		ret ="<a href='"+ranking["short"]
-#	else:
-#		ret ="<a href='"+host+"usr/"+ranking["bbs_key"]
-#	ret+="/"+ranking["thread_key"]
-#	ret+=".html'><IMG SRC='"+host+"thumbnail/"+ranking["image_key"]
-#	if(ranking["moper"]=="True"):
-#		ret+=".gif"
-#	else:
-#		ret+=".jpg"
-#	ret+="' BORDER=0 ALT='"+ranking["title"]+"' WIDTH=60px HEIGHT=60px></a>"
-#	return ret
-
-#def ranking_text_with_except(ranking,host):
-#	app=ranking["applause"]
-#	if(app=="None"):
-#		app="0"
-#	ret=""+ranking["title"]+"<BR>"+ranking["author"]+"<BR>"+ranking["create_date"]+"<BR>"+app+"拍手<BR>";
-#	return ret
 
 def reply_with_except(entry,host):
 	try:
@@ -83,9 +62,9 @@ def reply_with_except(entry,host):
 		else:
 			ret+=str(entry.thread_key.key())
 		ret+=".html' class='imagelink'>"
-		ret+="<IMG SRC='"+host+"thumbnail/"+str(entry.illust_reply_image_key.key())+".jpg' BORDER=0 "
-		ret+="ALT='"+str(entry.thread_key.title)+"'>"
-		ret+="</A>"
+		ret+="<img src='"+host+"thumbnail/"+str(entry.illust_reply_image_key.key())+".jpg' border='0' "
+		ret+="alt='"+str(entry.thread_key.title)+"'>/"
+		ret+="</a>"
 	except:
 		return ""#+traceback.format_exc()
 	return ret
@@ -102,7 +81,7 @@ def comment_with_except_no_hour(entry,host):
 
 def comment_with_except_core(entry,host,no_hour):
 	try:
-		ret="<A HREF='"+host;
+		ret="<a href='"+host;
 		if(entry["short"] and entry["short"]!="None") :
 			ret+=str(entry["short"])
 		else:
@@ -112,7 +91,7 @@ def comment_with_except_core(entry,host,no_hour):
 		ret+=".html' class='decnone'>"
 		title=str(entry["thread_title"])
 		ret+=title
-		ret+="</A><BR>（"
+		ret+="</a><br/>（"
 		if(no_hour):
 			ret+=time_JST_format(entry["date"],11);
 		else:
@@ -124,12 +103,12 @@ def comment_with_except_core(entry,host,no_hour):
 
 def admin_comment_with_except(entry):
 	try:
-		ret="<A HREF='./usr/"
+		ret="<a href='./usr/"
 		ret+=str(entry.bbs_key.key())+"/"
 		ret+=str(entry.thread_key.key())
 		ret+=".html'>"
 		ret+=entry.content
-		ret+="</A>"
+		ret+="</a>"
 	except:
 		return "error "+traceback.format_exc()
 	return ret
@@ -156,7 +135,7 @@ def show_bookmark(thread_key):
 	text+="' class='imagelink'><IMG SRC='"
 	text+="./thumbnail/"
 	text+=""+thread.image
-	text+=".jpg' BORDER=0></A>";
+	text+=".jpg' border='0'/></a>";
 	return text
 
 def show_bookmark_bbs(bbs_key):
@@ -173,9 +152,9 @@ def show_bookmark_bbs(bbs_key):
 		text+="./usr/"+str(bbs.key())+"/"
 	text+="' >";
 	text+=bbs.bbs_name
-	text+="</A>"
+	text+="</a>"
 	
-	text+="<a href='show_bookmark?bbs_key="+str(bbs.key())+"'</A>　"
+	text+="<a href='show_bookmark?bbs_key="+str(bbs.key())+"'</a>　"
 	if(bbs.bookmark_count):
 		text+="<small>("+str(bbs.bookmark_count)
 		if(bbs.bookmark_count==1):
@@ -183,7 +162,7 @@ def show_bookmark_bbs(bbs_key):
 		else:
 			text+="users"
 		text+=")</small>"
-	text+="</A>"
+	text+="</a>"
 	
 	return text
 
@@ -229,7 +208,7 @@ def show_app_core(app,size,size2):
 	txt='<div style="background-color:#ffffff;width:'+str(size2)+'px;min-height:'+str(size2)+'px;padding:4px;float:left;">'
 	txt+='<a href="app?mode=play&app_id='+app.app_id+'"><img src="app?mode=icon&app_id='+app.app_id+'" width='+str(size)+'px height='+str(size)+'px class="app_icon"></a>'
 	style='margin-bottom: 2px;font-size: .85em;font-weight: bold;font-family: arial,helvetica,osaka,\'MS PGothic\',sans-serif;color: #004B91;'
-	txt+='<p style="margin-top:4px;"><a href="app?mode=play&app_id='+app.app_id+'" style="'+style+'">'+app.app_name+'</a><BR>'
+	txt+='<p style="margin-top:4px;"><a href="app?mode=play&app_id='+app.app_id+'" style="'+style+'">'+app.app_name+'</a><br/>'
 	txt+='<a href="show_bookmark?app_key='+str(app.key())+'">';
 	txt+=str(app.bookmark_count)
 	if(app.bookmark_count==1):
@@ -254,7 +233,7 @@ def show_app_mypage(app):
 	except:
 		return "アプリ取得エラー";
 	txt+='<a href="app?mode=play&app_id='+app.app_id+'">';
-	txt+='<img src="app?mode=icon&app_id='+app.app_id+'" width=50px height=50px class="app_icon_small">'
+	txt+='<img src="app?mode=icon&app_id='+app.app_id+'" width="50px" height="50px" class="app_icon_small">'
 	#txt+=str(app.app_name)
 	txt+='</a>';
 	return txt
@@ -274,21 +253,21 @@ def user_id_to_user_name(user_id):
 def author_name_core(user_id,homepage_addr,author,host):
 	txt=""
 	if user_id:
-		txt+='<A HREF="'
+		txt+='<a href="'
 		txt+=host
 		txt+='mypage?user_id='
 		txt+=user_id
 		txt+='" class="g-button mini" style="color:#444;">'
 		txt+=author
-		txt+='</A>'
+		txt+='</a>'
 		return txt
 	if homepage_addr:
-		txt+='<A HREF="'
+		txt+='<a href="'
 		txt+=''+homepage_addr
 		txt+='"'
 		txt+=' class="g-button mini" style="color:#444;">'
 		txt+=author
-		txt+="</A>"
+		txt+="</a>"
 		return txt
 	return author
 
@@ -325,8 +304,6 @@ def new_feed_count(obj):
 register.filter(time_JST)
 register.filter(time_JST_format)
 register.filter(time_JST_short)
-#register.filter(ranking_image_with_except)
-#register.filter(ranking_text_with_except)
 register.filter(comment_with_except)
 register.filter(comment_with_except_no_hour)
 register.filter(reply_with_except)
