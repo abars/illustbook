038f282169e118caff83eb8e290abb2865447247
abars <abarsceo@gmail.com>
2013-05-12 20:06:18 +0900

    treat deleted thread

--- myapp/ApiObject.py
+++ myapp/ApiObject.py
@@ -517,8 +517,10 @@ class ApiObject(webapp.RequestHandler):
 			bbs_object=bbs_hash[bbs_object_key]
 			if(bbs_object):
 				bbs=ApiObject.create_bbs_object(req,bbs_object)
-				if(not bbs):
-					bbs={"title":"","bbs_url":"","bookmark":0,"key":"","thumbnail_url":""}	#deleted bbs
+			else:
+				bbs=None
+			if(not bbs):
+				bbs={"title":"","bbs_url":""}	#deleted bbs
 			
 		#イベント発生スレッド取得
 		thread_object_key=StackFeedData.thread_key.get_value_for_datastore(feed)
@@ -528,6 +530,10 @@ class ApiObject(webapp.RequestHandler):
 			if(thread_object):
 				only_image=False
 				thread=ApiObject._create_thread_object_core(req,thread_object,bbs_object,only_image)
+			else:
+				thread=None
+			if(not thread):
+				thread={"title":"","thread_url":""}	#deleted thread
 
 		#発生日取得
 		create_date=TimeProgress.get_date_diff_str(feed.create_date,"")
