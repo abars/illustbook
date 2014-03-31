b59e9f6fe147dce734b83c59a28af78130f04560
abars <abarsceo@gmail.com>
2014-04-01 08:48:03 +0900

    fix info layout

--- custom_filter.py
+++ custom_filter.py
@@ -289,7 +289,7 @@ def user_id_to_user_name(user_id):
 
 def author_name_core(user_id,homepage_addr,author,host):
 	txt=""
-	style="margin-left:0px;margin-bottom:2px;"
+	style=""#"margin-left:0px;margin-bottom:2px;"
 	if user_id:
 		txt+='<a href="'
 		txt+=host
