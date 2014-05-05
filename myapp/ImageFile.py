a8521438512b40ad7295730e2c4276d040c1dba0
abars <abarsceo@gmail.com>
2014-05-06 01:10:27 +0900

    allow direct image link

--- myapp/ImageFile.py
+++ myapp/ImageFile.py
@@ -221,11 +221,11 @@ class ImageFile (webapp.RequestHandler):
 	@staticmethod
 	def serve_file(p_self,path,type_name,tag):
 		#直リンクの禁止
-		if(not p_self.request.get("force")):
-			if(not tag=="tolot" and not tag=="tile"):
-				if(ImageFile.is_direct_access(p_self)):
-					p_self.error(403)
-					return
+		#if(not p_self.request.get("force")):
+		#	if(not tag=="tolot" and not tag=="tile"):
+		#		if(ImageFile.is_direct_access(p_self)):
+		#			p_self.error(403)
+		#			return
 
 		#キャッシュヒット判定
 
