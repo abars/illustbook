bdbd8c6b0938380edcc3fb907bd19ed11891c2ba
abars <abarsceo@gmail.com>
2014-05-11 18:23:28 +0900

    scoring search api by date

--- myapp/SearchThread.py
+++ myapp/SearchThread.py
@@ -11,6 +11,7 @@ import os
 import sys
 import re
 import datetime
+import time;
 import random
 import logging
 import urllib
@@ -57,8 +58,13 @@ class SearchThread(webapp.RequestHandler):
 				search.NumberField(name='bookmark', value=SearchThread._number_validate(thread.bookmark_count)),
 				search.NumberField(name='comment', value=SearchThread._number_validate(thread.comment_cnt)),
 				search.HtmlField(name='summary', value=thread.summary),
-				search.DateField(name='date', value=thread.create_date)
+				search.DateField(name='date', value=thread.create_date),
+				search.NumberField(name='sec', value=SearchThread._get_sec(thread.create_date))
 			])
+
+	@staticmethod
+	def _get_sec(now):
+		return int(time.mktime(now.timetuple()))
 
 	@staticmethod
 	def _create_document_entry(entry):
@@ -94,6 +100,7 @@ class SearchThread(webapp.RequestHandler):
 				search.TextField(name='thread_key', value=thread_key),
 				search.TextField(name='bbs_key', value=bbs_key),
 				search.DateField(name='date', value=date),
+				search.NumberField(name='sec', value=SearchThread._get_sec(date)),
 				search.NumberField(name='comment_no', value=comment_no)
 			])
 
@@ -127,10 +134,12 @@ class SearchThread(webapp.RequestHandler):
 
 	@staticmethod
 	def search(query,page,unit,index):
+		now_sec=SearchThread._get_sec(datetime.datetime.now())
+		reduct='(1+('+str(now_sec)+'-sec)/(3600*24*30))';	#一ヶ月で半分のスコアにする
+
 		sort_options = search.SortOptions(
 			expressions=[
-#				search.SortExpression(expression='date', direction=search.SortExpression.DESCENDING, default_value=0)
-				search.SortExpression(expression='applause+bookmark*5', direction=search.SortExpression.DESCENDING, default_value=0)
+				search.SortExpression(expression='(applause+bookmark*5)/'+reduct, direction=search.SortExpression.DESCENDING, default_value=0)
 			],limit=1000)
 		options = search.QueryOptions(
 			limit=unit,
