5ad3eb344a134a2e7ea2c3eb827b52bf83b18710
abars <abarsceo@gmail.com>
2014-05-17 20:35:31 +0900

    revert portal.html

--- myapp/Chat.py
+++ myapp/Chat.py
@@ -319,7 +319,7 @@ class Chat(webapp.RequestHandler):
 			'user_name': user_name,
 			'is_english':CssDesign.is_english(self)
 		}
-		path = '/html/chat.html'
+		path = '/html/portal.html'
 		self.response.out.write(template_select.render(path, template_values))
 	
 	#経過時間取得
